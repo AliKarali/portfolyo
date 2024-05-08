acef5a270d10374081daea7fa9b450fec51d2d04
AliKarali <burakali2245@hotmail.com>
2024-05-08 06:41:56 +0300

    heroku

--- portfolyo/settings.py
+++ portfolyo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ok+&3da)di4jms1le$^rmku5&(zgas417sdi*nb*(pvtf)n-3u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
